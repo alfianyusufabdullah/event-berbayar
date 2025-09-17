451d7c03d1622372e155bbfe85831c0efe46f64d
Alfian Yusuf Abdullah <alfian@dicoding.com>
2025-09-18 05:44:49 +0700

    feat: add appUrl

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     CF_ACCESS_CLIENT_ID: process.env.CF_ACCESS_CLIENT_ID,
     CF_ACCESS_CLIENT_SECRET: process.env.CF_ACCESS_CLIENT_SECRET,
+    APP_URL: process.env.APP_URL,
   },
 };
 
