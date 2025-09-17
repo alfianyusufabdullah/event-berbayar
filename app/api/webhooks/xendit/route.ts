45f1a5f7862a6c053d3ed36abf8195ec4f8dba59
Alfian Yusuf Abdullah <alfian@dicoding.com>
2025-09-18 05:45:46 +0700

    feat: update update with webhook payload

--- app/api/webhooks/xendit/route.ts
+++ app/api/webhooks/xendit/route.ts
@@ -1,10 +1,15 @@
+import { updatePayment } from '@/lib/api';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    console.log('Received Xendit webhook:', body);
-    return NextResponse.json({ message: 'Webhook received successfully' }, { status: 200 });
+    const updatedPayment = await updatePayment(body);
+    if (updatedPayment.error) {
+      return NextResponse.json({ message: 'Failed to process webhook' }, { status: 400 });
+    }
+
+    return NextResponse.json({ message: 'Webhook processed successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error processing Xendit webhook:', error);
     return NextResponse.json({ message: 'Error processing webhook' }, { status: 500 });
